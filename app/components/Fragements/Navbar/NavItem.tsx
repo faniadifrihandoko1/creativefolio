8a71f1183b7e9d04a40299fd64187b1fbe67715b
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2024-11-10 23:11:33 +0700

    refactor: hover navbar items

--- app/components/Fragements/Navbar/NavItem.tsx
+++ app/components/Fragements/Navbar/NavItem.tsx
@@ -18,13 +18,13 @@ const NavItem = ({ name, path, onClick, classname, isMobile }: Props) => {
     <li className={classname}>
       <Link href={path}>
         <div
-          className={`block pb-1 md:relative md:group font-medium `}
+          className={`block pb-1 md:relative md:group font-medium hover:bg-gray-100 hover:text-gray-900 md:hover:bg-transparent md:hover:text-indigo-600 md:dark:hover:text-indigo-500`}
           onClick={isMobile ? onClick : undefined}
         >
           {name}
           <span
             className={`absolute bottom-0 left-0 h-[2px] bg-red-500 transition-all duration-500 ease-in-out ${
-              isActive ? "w-full" : "w-0 md:group-hover:w-full"
+              isActive ? "w-full" : "w-0 "
             }`}
           ></span>
         </div>
