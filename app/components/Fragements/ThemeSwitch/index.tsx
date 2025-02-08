8fe924d8da72224d4562c3f6963eed992ef97b5c
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-02-08 19:09:06 +0700

    fix: toogle dark mode

--- app/components/Fragements/ThemeSwitch/index.tsx
+++ app/components/Fragements/ThemeSwitch/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useTheme } from "next-themes";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { FiSun, FiMoon } from "react-icons/fi";
 import Button from "../../Elements/Button";
 
 const ThemeSwitch = () => {
@@ -10,16 +11,22 @@ const ThemeSwitch = () => {
   useEffect(() => {
     setMounted(true);
   }, []);
-  if (!mounted) {
-    return null;
-  }
+
+  if (!mounted) return null;
+
   return (
-    <Button
-      classname="rounded-full text-xl md:text-2xl  transition duration-300  hover:rotate-180  hover:text-slate-300"
-      onclick={() => setTheme(theme === "light" ? "dark" : "light")}
-    >
-      {theme === "light" ? "🌞" : "🌚"}
-    </Button>
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        checked={theme === "dark"}
+        onChange={() => setTheme(theme === "light" ? "dark" : "light")}
+        className="sr-only peer"
+      />
+      <div className="w-15 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-300 dark:peer-focus:ring-gray-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-5 peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[6px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 flex items-center gap-2 justify-between px-2">
+        <FiSun className="text-yellow-500" size={16} />
+        <FiMoon className="text-gray-500" size={16} />
+      </div>
+    </label>
   );
 };
 
