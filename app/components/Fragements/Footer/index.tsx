572eeb712e55a9c495264950edd0ce318e59c14d
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-07-26 23:44:29 +0700

    feat(footer): update years

--- app/components/Fragements/Footer/index.tsx
+++ app/components/Fragements/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
       className="text-center text-sm md:text-base py-4 px-2  w-full "
       style={{ zIndex: 50 }}
     >
-      © 2024 Fani Adi Frihandoko. All rights reserved.
+      © 2025 Fani Adi Frihandoko. All rights reserved.
     </footer>
   );
 };
