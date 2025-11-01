f5e4e465679e2713e2e1aa07a7ed4df79227cf45
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-11-01 18:59:32 +0700

    update(componen-navbar-cms): edit component navbar cms

--- app/components/Fragements/Navbar/NavbarCMS.tsx
+++ app/components/Fragements/Navbar/NavbarCMS.tsx
@@ -8,9 +8,12 @@ const NavbarCMS = () => {
     // navbar cms dengan link dan text
     <div className=" flex  flex-row md:justify-between md:items-center px-5 py-3 w-full transition-all duration-300 backdrop-blur-3xl bg-white/10">
       <Link href={"/"}>
-        <TiArrowBack size={25} className="hover:text-green-500" />
+        <TiArrowBack size={25} className="hover:text-green-500" />{" "}
+        {/* icon back dengan size 25 */}
       </Link>
       <h1 className="text-2xl font-extrabold text-indigo-950 dark:text-bgBody">
+        {" "}
+        {/* text dengan font size 24 */}
         Fan<span className="text-green-500">dev.</span>
       </h1>
     </div>
