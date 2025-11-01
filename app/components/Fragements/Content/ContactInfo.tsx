615b2c02eab5d0738ebc3b9ee0e627c4635b5749
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-11-01 18:57:02 +0700

    update(componen-contact-info): edit component contact info

--- app/components/Fragements/Content/ContactInfo.tsx
+++ app/components/Fragements/Content/ContactInfo.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import ContactItem from "./ContactItem";
 import { FaGithub } from "react-icons/fa";
@@ -9,16 +9,18 @@ const ContactInfo = () => {
     // contact info dengan text dan link
     <div className="mt-5 flex flex-col gap-2 justify-center items-center">
       <p>Find me on</p> {/* text dengan font size 14 */}
-      <div className="flex  md:flex-row gap-2 justify-center items-center"> {/* flex container dengan gap 2 */}
+      <div className="flex  md:flex-row gap-2 justify-center items-center">
+        {" "}
+        {/* flex container dengan gap 2 */}
         <ContactItem
-          icon={<FaGithub size={25} />} {/* icon github dengan size 25 */}
-          text="github"
-          link="https://github.com/faniadifrihandoko1" {/* link github */}
+          icon={<FaGithub size={25} />} /* icon github dengan size 25 */
+          text="github" /* text dengan font size 14 */
+          link="https://github.com/faniadifrihandoko1" /* link github */
         />
         <ContactItem
-          icon={<MdEmail size={25} />} {/* icon email dengan size 25 */}
-          text="email" {/* text dengan font size 14 */}
-          link="mailto:faniadifrihandoko1@gmail.com" {/* link email */}
+          icon={<MdEmail size={25} />} /* icon email dengan size 25 */
+          text="email" /* text dengan font size 14 */
+          link="mailto:faniadifrihandoko1@gmail.com" /* link email */
         />
       </div>
     </div>
