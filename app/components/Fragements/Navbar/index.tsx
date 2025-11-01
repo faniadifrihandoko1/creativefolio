942419fca4af74399055f76da15414601413f5de
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-11-01 19:00:05 +0700

    update(layout-navbar): edit layout navbar

--- app/components/Fragements/Navbar/index.tsx
+++ app/components/Fragements/Navbar/index.tsx
@@ -62,6 +62,7 @@ const Navbar = ({ isOpen, setIsOpen }: Props) => {
   }, []);
 
   return (
+    // navbar dengan background color dan shadow
     <div
       className={`fixed top-0 left-0 py-5 right-0 z-10 transition-all w-full duration-300 ${
         isScrolled
