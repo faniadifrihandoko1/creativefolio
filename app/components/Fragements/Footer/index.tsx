1d206d5128ad8b98635312f896bebb7b2075dbf4
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-11-02 19:09:31 +0700

    style(footer): change style footer

--- app/components/Fragements/Footer/index.tsx
+++ app/components/Fragements/Footer/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 const Footer = () => {
   return (
-    // footer dengan copyright dan tahun
     <footer
       className="text-center text-sm md:text-base py-4 px-2  w-full "
       style={{ zIndex: 50 }}
