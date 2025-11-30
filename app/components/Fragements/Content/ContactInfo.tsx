05ece542546a0e9c4cb4209d26ad45af5e007ea4
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-11-30 23:42:17 +0700

    style(contact-info): re-style component contact info

--- app/components/Fragements/Content/ContactInfo.tsx
+++ app/components/Fragements/Content/ContactInfo.tsx
@@ -8,11 +8,13 @@ const ContactInfo = () => {
     <div className="mt-5 flex flex-col gap-2 justify-center items-center">
       <p>Find me on</p>
       <div className="flex  md:flex-row gap-2 justify-center items-center">
+        {/* github */}
         <ContactItem
           icon={<FaGithub size={25} />}
           text="github"
           link="https://github.com/faniadifrihandoko1"
         />
+        {/* email */}
         <ContactItem
           icon={<MdEmail size={25} />}
           text="email"
