6dc14524c5ee8225acf17815b8ea3de2cef1ebc1
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-12-13 23:53:40 +0700

    feat(Desc): update desc

--- app/components/Fragements/Content/ContactItem.tsx
+++ app/components/Fragements/Content/ContactItem.tsx
@@ -13,7 +13,6 @@ const ContactItem = ({ icon, text, link }: Props) => {
       href={link}
       className="border flex gap-1 justify-center items-center border-slate-950 dark:border-bgBody p-1 hover:bg-neutral-400"
     >
-      {/* icon */}
       {icon}
       {/* text */}
       <p>{text}</p>
