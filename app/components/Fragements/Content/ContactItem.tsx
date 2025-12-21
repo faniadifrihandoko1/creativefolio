9a4fe01ee74e216ba3a010bb6540a9b3d5c9a6d8
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2025-12-21 23:59:30 +0700

    style(component-contact): change style component contact

--- app/components/Fragements/Content/ContactItem.tsx
+++ app/components/Fragements/Content/ContactItem.tsx
@@ -13,8 +13,13 @@ const ContactItem = ({ icon, text, link }: Props) => {
       href={link}
       className="border flex gap-1 justify-center items-center border-slate-950 dark:border-bgBody p-1 hover:bg-neutral-400"
     >
+      {/* start contact item */}
+      {/* start icon */}
       {icon}
+      {/* end icon */}
+      {/* start text */}
       <p>{text}</p>
+      {/* end contact item */}
     </Link>
   );
 };
