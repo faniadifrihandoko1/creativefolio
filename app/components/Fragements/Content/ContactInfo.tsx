0d05116716b7dd8f8f44534d41f51d1100ad14aa
faniadifrihandoko1 <faniadifrihandoko01@gmail.com>
2026-09-19 21:11:57 +0700

    feat(content): update comoponent contact info

--- app/components/Fragements/Content/ContactInfo.tsx
+++ app/components/Fragements/Content/ContactInfo.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React from "react";
-import ContactItem from "./ContactItem";
 import { FaGithub } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
+import ContactItem from "./ContactItem";
 const ContactInfo = () => {
+
   return (
     <div className="mt-5 flex flex-col gap-2 justify-center items-center">
       <p>Find me on</p>
